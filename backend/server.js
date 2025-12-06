79e8a97852a8043b5612373fd374edb2e8e0404e
RohitVerma2003 <rvermajio78300@gmail.com>
2025-12-06 23:02:21 +0530

    Deployment errors fix

--- backend/server.js
+++ backend/server.js
@@ -26,8 +26,8 @@ app.use("/api/user", userRoute);
 app.use("/api/items", itemRoute);
 app.use("/api/cart", cartRoute);
 
-app.use(express.static(path.join(__dirname , "/frontend/dist")))
-app.get("*" , (req , res)=>{
+app.use(express.static(path.join(__dirname , "frontend" , "dist")))
+app.get("/*" , (req , res)=>{
     res.sendFile(path.join(__dirname , "frontend" , "dist" , "index.html"))
 })
 
